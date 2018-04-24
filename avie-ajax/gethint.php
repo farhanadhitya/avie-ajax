1d6919816cb4c8edc21e519857e5c5629c4234ce
aviyudhistira <38452976+aviyudhistira@users.noreply.github.com>
2018-04-24 14:49:33 +0700

    Done

---
+++ avie-ajax/gethint.php
@@ -0,0 +1,34 @@
+<?php
+// array with names
+$a[] = "aviyudhistira";
+$a[] = "odettetank";
+$a[] = "kurnia";
+$a[] = "farhan";
+$a[] = "asdaniel";
+$a[] = "toni";
+
+$q =$_REQUEST["q"];
+
+$hint="";
+
+if($q !==""){
+	$q = strtolower($q);
+	$len=strlen($q);
+	foreach($a as $name){
+		if (stristr($q,substr($name,0, $len))) {
+		if ($hint ===""){
+			$hint = $name;
+		}else{
+			$hint .=", $name";
+
+		}
+
+		}
+	}
+}
+
+
+//output "no suggestion" if no hint was found or output correct values
+echo $hint === "" ? "Nang Adong(TIDAK ADA)" : $hint;
+?>
+
